11e1c76292be5888b00bdc2796ec84920effa3e5
Nico Hagelberg <nico.hagelberg@gmail.com>
2016-03-01 09:11:55 +0200

    Paranneltu loginin toimivuutta

--- pizzaSivusto/src/kayttajaDao/KayttajaDAO.java
+++ pizzaSivusto/src/kayttajaDao/KayttajaDAO.java
@@ -17,6 +17,9 @@ public class KayttajaDAO {
 
 	public Kayttaja kirjaudu(String kayttajatunnus, String salasana) {
 
+		// Käyttäjäolion luonti
+		Kayttaja kayttaja = new Kayttaja();
+
 		// Yhteyden määritys
 		Yhteys yhteys = new Yhteys();
 		Kysely kysely = new Kysely(yhteys.getYhteys());
@@ -38,51 +41,55 @@ public class KayttajaDAO {
 			suola = (String) kayttajaMappi.get("suola");
 		}
 
-		// Salasana tiivisteeksi
-		Tiiviste tiiviste = new Tiiviste();
-		String salasanatiiviste = null;
-		try {
-			salasanatiiviste = tiiviste.salaa(salasana, suola, 1);
-		} catch (Exception ex) {
-			System.out.println("Virhe salasanatiivisteen käsittelyssä - " + ex);
+		// Ei jatketa, jos käyttäjää ei löydy
+		if (suola == null) {
+			System.out.println("Käyttäjää ei löydy");
+			return kayttaja;
+		} else {
+
+			// Salasana tiivisteeksi
+			Tiiviste tiiviste = new Tiiviste();
+			String salasanatiiviste = null;
+			try {
+				salasanatiiviste = tiiviste.salaa(salasana, suola, 1);
+			} catch (Exception ex) {
+				System.out.println("Virhe salasanatiivisteen käsittelyssä - " + ex);
+			}
+
+			// Käyttäjän haku tietokannasta
+			String sqlkysely = "SELECT id, tunnus, etunimi, sukunimi, puhelin FROM Kayttajat WHERE tunnus = ? AND salasana = ?";
+			parametrit.add(salasanatiiviste);
+
+			kysely.suoritaYksiKyselyParam(sqlkysely, parametrit);
+			tulokset = kysely.getTulokset();
+
+			// Testaillaan
+
+			iteraattori = kysely.getTulokset().iterator();
+
+			while (iteraattori.hasNext()) {
+				HashMap kayttajaMappi = (HashMap) iteraattori.next();
+				String idString = (String) kayttajaMappi.get("id");
+				String tunnusKanta = (String) kayttajaMappi.get("tunnus");
+				String etunimiKanta = (String) kayttajaMappi.get("etunimi");
+				String sukunimiKanta = (String) kayttajaMappi.get("sukunimi");
+				String puhelinKanta = (String) kayttajaMappi.get("puhelin");
+				int idKanta = Integer.parseInt(idString);
+
+				// Oliolle attribuutit
+				kayttaja.setTunnus(tunnusKanta);
+				kayttaja.setId(idKanta);
+				kayttaja.setEtunimi(etunimiKanta);
+				kayttaja.setSukunimi(sukunimiKanta);
+				kayttaja.setPuhelin(puhelinKanta);
+			}
+
+			// Yhteyden sulkeminen
+			yhteys.suljeYhteys();
+
+			// Käyttäjän palautus
+			return kayttaja;
 		}
-
-		// Käyttäjän haku tietokannasta
-		String sqlkysely = "SELECT id, tunnus, etunimi, sukunimi, puhelin FROM Kayttajat WHERE tunnus = ? AND salasana = ?";
-		parametrit.add(salasanatiiviste);
-
-		kysely.suoritaYksiKyselyParam(sqlkysely, parametrit);
-		tulokset = kysely.getTulokset();
-		
-		// Testaillaan
-
-		iteraattori = kysely.getTulokset().iterator();
-
-		// Käyttäjäolion luonti
-		Kayttaja kayttaja = new Kayttaja();
-
-		while (iteraattori.hasNext()) {
-			HashMap kayttajaMappi = (HashMap) iteraattori.next();
-			String idString = (String) kayttajaMappi.get("id");
-			String tunnusKanta = (String) kayttajaMappi.get("tunnus");
-			String etunimiKanta = (String) kayttajaMappi.get("etunimi");
-			String sukunimiKanta = (String) kayttajaMappi.get("sukunimi");
-			String puhelinKanta = (String) kayttajaMappi.get("puhelin");
-			int idKanta = Integer.parseInt(idString);
-
-			// Oliolle attribuutit
-			kayttaja.setTunnus(tunnusKanta);
-			kayttaja.setId(idKanta);
-			kayttaja.setEtunimi(etunimiKanta);
-			kayttaja.setSukunimi(sukunimiKanta);
-			kayttaja.setPuhelin(puhelinKanta);
-		}
-
-		// Yhteyden sulkeminen
-		yhteys.suljeYhteys();
-
-		// Käyttäjän palautus
-		return kayttaja;
 	}
 
 	// Väliaikainen käyttäjätietojen haku
